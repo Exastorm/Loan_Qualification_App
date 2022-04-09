c363ce26cb42747af7e843c490b6efc4fe7aa005
Christian Chavez <34184270+Exastorm@users.noreply.github.com>
2022-04-09 01:23:23 -0700

    + function to display qualifying loans on screen

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ Returns the bank data from the data rate sheet CSV file.
 def load_bank_data():
 
     # global input_path # not needed
-    input_path = questionary.text("Please enter the file path to a .csv rate-sheet: ").ask()
+    input_path = questionary.text("Please enter the file path to a .csv rate-sheet:").ask()
     input_path = Path(input_path)
     while not input_path.exists():
 
         print(f"Sorry, you have entered an incorrect path or filename: '{input_path}' cannot be found...")
-        input_path = questionary.text("Please enter the file path to a .csv rate-sheet: ").ask()
+        input_path = questionary.text("Please enter the file path to a .csv rate-sheet:").ask()
         input_path = Path(input_path)
         
     return load_csv(input_path)
@@ -39,11 +39,11 @@ Returns the applicant's financial information.
 
 def get_applicant_info():
 
-    credit_score = questionary.text("What's your credit score? ").ask()
-    debt = questionary.text("What's your current amount of monthly debt? ").ask()
-    income = questionary.text("What's your total monthly income? ").ask()
-    loan_amount = questionary.text("What's your desired loan amount? ").ask()
-    home_value = questionary.text("What's your home value? ").ask()
+    credit_score = questionary.text("What's your credit score?").ask()
+    debt = questionary.text("What's your current amount of monthly debt? $").ask()
+    income = questionary.text("What's your total monthly income? $").ask()
+    loan_amount = questionary.text("What's your desired loan amount? $").ask()
+    home_value = questionary.text("What's your home value? $").ask()
 
     credit_score = int(credit_score)
     debt = float(debt)
@@ -78,11 +78,11 @@ def find_qualifying_loans(bank_data_, credit_score_, debt_, income_, loan_, home
     
     # Calculate the monthly debt ratio
     monthly_debt_ratio = calculate_monthly_debt_ratio(debt_, income_)
-    print(f"The monthly debt to income ratio is {monthly_debt_ratio:.02f}")
+    print(f"\n       Your monthly debt to income ratio is {int(monthly_debt_ratio * 100)}%")
 
     # Calculate loan to value ratio
     loan_to_value_ratio = calculate_loan_to_value_ratio(loan_, home_value_)
-    print(f"The loan to value ratio is {loan_to_value_ratio:.02f}")
+    print(f"\n       Your loan to value ratio is {int(loan_to_value_ratio * 100)}%")
 
     # Run qualification filters
     bank_data_filtered = filter_max_loan_size(loan_, bank_data_)
@@ -91,20 +91,36 @@ def find_qualifying_loans(bank_data_, credit_score_, debt_, income_, loan_, home
     bank_data_filtered = filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
 
     if len(bank_data_filtered) == 0:
-        print("Sorry, no qualifying loans are available at this time.")
+        print("\n       Sorry, no qualifying loans are available at this time.\n")
     elif len(bank_data_filtered) == 1:
-        print("There is 1 qualifying loan available.")
+        print("\n       There is 1 qualifying loan available.\n")
     else:
-        print(f"There are {len(bank_data_filtered)} qualifying loans available.")
+        print(f"\n       There are {len(bank_data_filtered)} qualifying loans available.\n")
 
     return bank_data_filtered
 
 """Save the qualifying loans to a CSV file.
 Args: qualifying_loans_ (list of lists): The qualifying bank loans.
 """
-# @TODO: Complete the usability dialog for saving the CSV Files.
+# @TODO: Complete the usability dialog for saving the CSV Files
+# FIRST I'm adding an additional function to optionally display results
 
+def view_qualifying_loans(qualifying_loans_): 
+    view = questionary.text("Would you like to view your qualifying loan information?").ask()
+    x = 1
+    while x == 1:
+        if str.lower(view) == "yes" or "y":
+            for each_loan in qualifying_loans_:
+                print(*each_loan)
+            x = 0
+        elif str.lower(view) == "no" or "n":
+            print()
+            x = 0
+        else:
+            view = questionary.text("Please respond 'yes' or 'no'...").ask()
+    
 def save_qualifying_loans(qualifying_loans_):
+    # view = questionary.text("Would you like to download your qualifying loan information?").ask()
     output_path = Path("qualifying_loans.csv")
     with output_path.open("w", newline="") as csv_file:
         csvwriter = csv.writer(csv_file, delimiter = ",")
@@ -125,10 +141,15 @@ def run():
         bank_data, credit_score, debt, income, loan_amount, home_value
     )
 
-    # Save qualifying loans
-    save_qualifying_loans(qualifying_loans)
+    if len(qualifying_loans) > 0:
 
-    print("Thank you for using Loan Qualification App by Exastorm.")
-    
+        # Optional: View qualifying loans
+        view_qualifying_loans(qualifying_loans)
+        
+        # Optional: Save qualifying loans
+        save_qualifying_loans(qualifying_loans)
+
+    print("\n   Thank you for using the Loan Qualification App by Exastorm.\n")
+
 if __name__ == "__main__":
     fire.Fire(run)
